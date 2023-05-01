251529f6a6f3782bda82d634085ad2338748d8b1
rivka <rarivkale@gmail.com>
2023-05-01 11:47:13 +0300

    Updates

--- precache-manifest.3f95ef24168b24a5b6b4a9253c82b2fc.js
+++ precache-manifest.3f95ef24168b24a5b6b4a9253c82b2fc.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "085fcfa428c840779fcb",
-    "url": "/gemolav/static/css/main.08ea1b6d.chunk.css"
+    "revision": "a70f62d8d2b46aadc8a5",
+    "url": "/gemolav/static/css/main.5d856951.chunk.css"
   },
   {
-    "revision": "085fcfa428c840779fcb",
-    "url": "/gemolav/static/js/main.085fcfa4.chunk.js"
+    "revision": "a70f62d8d2b46aadc8a5",
+    "url": "/gemolav/static/js/main.a70f62d8.chunk.js"
   },
   {
     "revision": "57fb75a2e8592a1edd30",
@@ -24,15 +24,15 @@ self.__precacheManifest = [
     "url": "/gemolav/static/js/runtime~main.665be21f.js"
   },
   {
-    "revision": "52196284de1fcb5b044f001a75482dba",
-    "url": "/gemolav/static/media/bootstrap-icons.52196284.woff"
-  },
-  {
     "revision": "7f477633ddd12f84284654f2a2e89b8a",
     "url": "/gemolav/static/media/bootstrap-icons.7f477633.woff2"
   },
   {
-    "revision": "96ccf16d80a158e71a8e03b6f207eaf9",
+    "revision": "52196284de1fcb5b044f001a75482dba",
+    "url": "/gemolav/static/media/bootstrap-icons.52196284.woff"
+  },
+  {
+    "revision": "8696c7d7aa3bd69907e9787f301e4641",
     "url": "/gemolav/index.html"
   }
 ];
